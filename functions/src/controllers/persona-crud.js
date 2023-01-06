ae359bce2cc44ff44b3aad7d596246ccff0aed73
JulieFrancois <66426143+JulieFrancois@users.noreply.github.com>
2023-01-06 10:56:36 +0100

    fix fonction postPersona

--- functions/src/controllers/persona-crud.js
+++ functions/src/controllers/persona-crud.js
@@ -8,18 +8,18 @@ const postPersona = handleRequest(async (body) => {
   return movie;
 });
 
-const getPersonas = handleRequest(async () => {
+const getPersona = handleRequest(async () => {
   const movies = await Persona.find();
   return movies;
 });
 
-const getPersonasById = handleRequest(async (id) => {
+const getPersonaById = handleRequest(async (id) => {
   const movie = await Persona.findById(id);
   if (!movie) throw new Error("Persona not found");
   return movie;
 });
 
-const updatePersonas = handleRequest(async (id, body) => {
+const updatePersona = handleRequest(async (id, body) => {
   const movieData = body;
   const movie = await Persona.findByIdAndUpdate(id, movieData, {
     new: true,
@@ -28,16 +28,16 @@ const updatePersonas = handleRequest(async (id, body) => {
   return movie;
 });
 
-const deletePersonas = handleRequest(async (id) => {
+const deletePersona = handleRequest(async (id) => {
   const movie = await Persona.findByIdAndDelete(id);
   if (!movie) throw new Error("Persona not found");
   return movie;
 });
 
 module.exports = {
-  postPersonas,
-  getPersonas,
-  getPersonasById,
-  updatePersonas,
-  deletePersonas,
+  postPersona,
+  getPersona,
+  getPersonaById,
+  updatePersona,
+  deletePersona,
 };
