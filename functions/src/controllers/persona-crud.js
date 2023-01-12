920ab0e75582288381e8a0004461fd3518242f23
JulieFrancois <66426143+JulieFrancois@users.noreply.github.com>
2023-01-12 14:42:49 +0100

    update

--- functions/src/controllers/persona-crud.js
+++ functions/src/controllers/persona-crud.js
@@ -3,41 +3,39 @@ const handleRequest = require("../utils/request");
 const Persona = require("../models/persona-model");
 
 const postPersona = handleRequest(async (body) => {
-  const movieData = body;
-  const movie = await Persona.create(movieData);
-  return movie;
+  const newModel = new Persona(body);
+  return await newModel.save();
 });
 
 const getPersona = handleRequest(async () => {
-  const movies = await Persona.find();
-  return movies;
+  const datas = await Persona.find();
+  return datas;
 });
 
-const getPersonaById = handleRequest(async (id) => {
-  const movie = await Persona.findById(id);
-  if (!movie) throw new Error("Persona not found");
-  return movie;
+const getPersonaById = handleRequest(async (body, id) => {
+  const data = await Persona.findById(id);
+  if (!data) throw new Error("Movie not found");
+  return data;
 });
 
-const updatePersona = handleRequest(async (id, body) => {
-  const movieData = body;
-  const movie = await Persona.findByIdAndUpdate(id, movieData, {
+const putPersonaById = handleRequest(async (body, id) => {
+  const data = await Persona.findByIdAndUpdate(id, body, {
     new: true,
   });
-  if (!movie) throw new Error("Persona not found");
-  return movie;
+  if (!data) throw new Error("Movie not found");
+  return data;
 });
 
-const deletePersona = handleRequest(async (id) => {
-  const movie = await Persona.findByIdAndDelete(id);
-  if (!movie) throw new Error("Persona not found");
-  return movie;
+const deletePersonaById = handleRequest(async (body, id) => {
+  const data = await Persona.findByIdAndDelete(id);
+  if (!data) throw new Error("Movie not found");
+  return data;
 });
 
 module.exports = {
   postPersona,
   getPersona,
-  getPersonaById,
-  updatePersona,
-  deletePersona,
+  getPersona,
+  putPersonaById,
+  deletePersonaById,
 };
